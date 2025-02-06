edc7d438ea730156e19dfeaba193ab40cfbc9803
MutaibaMunawar <mutaibamunawar@gmail.com>
2025-02-06 14:22:05 +0500

    main

--- src/app/components/protected/page.tsx
+++ src/app/components/protected/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-import  {useRouter}  from 'next/navigation'
-import React, { useEffect } from 'react'
-export default function ProtectedRoute({children}: {children: React.ReactNode} ){
-    const router = useRouter()
-    useEffect (() =>{
-        const isLoggedIn = localStorage.getItem('isLoggedIn')
-        if(!isLoggedIn){
-            router.push('/admin')
-        }
-    },[router]);
-    return <>
-    {children}
-    </>
+"use client"; // This makes it a Client Component
 
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (!isLoggedIn) {
+      router.push("/admin/login");
+    }
+  }, [router]);
+
+  return <>{children}</>;
 }
